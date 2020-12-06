3afc32514cdb7a1daaa65b92f48c00480b5d8048
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2020-12-06 15:23:39 +0800

    refactor weather station and line_notifacation to Observer Pattern

---
+++ line_notification.py
@@ -0,0 +1,66 @@
+import requests
+import logging
+from abc import ABC, abstractmethod
+
+from utils import trans_temp_kelvin_to_Celsius
+
+logger = logging.getLogger(__name__)
+
+
+class LineNotificationInterface(ABC):
+    LINE_URL = 'https://notify-api.line.me/api/notify'
+
+    def __init__(self, line_token):
+        self._line_token = line_token
+
+    @abstractmethod
+    def notify(self, msg):
+        headers = {'Authorization': 'Bearer ' + self._line_token}
+        payload = {'message': str(msg)}
+        response = requests.post(self.LINE_URL, headers=headers, params=payload)
+        return response.status_code
+
+    def enrich_message(self, msg):
+        pass
+
+
+class GeneralModelNotification(LineNotificationInterface):
+    def __init__(self, weather_station, user_data):
+        self.user_data = user_data
+        weather_station.register_weather_station(user_data.get('user'), self)
+        super().__init__(line_token=user_data.get('line_token'))
+
+    def notify(self, msg):
+        logger.info('user: {} GeneralModel notify message: {}'.format(
+            self.user_data.get('user', 'NAME_IS_NEEDED'), msg))
+        return super().notify(self.enrich_message(msg))
+
+    def enrich_message(self, msg):
+        return str({
+            'temperature': trans_temp_kelvin_to_Celsius(
+                msg.get('temperature', {}).get('temp', 'Null')),
+            'humidity': msg.get('humidity', 'Null'),
+            'rain': msg.get('rain') if msg.get('rain') else 0
+        })
+
+
+class PremiumModelNotification(LineNotificationInterface):
+    def __init__(self, weather_station, user_data):
+        self.user_data = user_data
+        weather_station.register_weather_station(user_data.get('user'), self)
+        super().__init__(line_token=user_data.get('line_token'))
+
+    def notify(self, msg):
+        logger.info('user: {} PremiumModel notify message: {}'.format(
+            self.user_data.get('user', 'NAME_IS_NEEDED'), msg))
+        return super().notify(self.enrich_message(msg))
+
+    def enrich_message(self, msg):
+        return str({
+            'temperature': trans_temp_kelvin_to_Celsius(
+                msg.get('temperature', {}).get('temp', 'Null')),
+            'humidity': msg.get('humidity', 'Null'),
+            'rain': msg.get('rain') if msg.get('rain') else 0,
+            'status': msg.get('status', 'Null'),
+            'wind speed': msg.get('wind', {}).get('speed', 'Null')
+        })
--- weather_station.py
+++ weather_station.py
@@ -2,10 +2,8 @@ import pyowm
 import json
 import logging
 from requests import Timeout
-import requests
 from os import path
-
-from utils import get_json_content
+from abc import ABC, abstractmethod
 
 
 CURRENT_PATH = path.dirname(path.abspath(__file__))
@@ -13,11 +11,40 @@ logger = logging.getLogger(__name__)
 LINE_URL = 'https://notify-api.line.me/api/notify'
 
 
-class WeatherStation():
+class Subject(ABC):
+    @abstractmethod
+    def register_weather_station(self, user, model):
+        pass
+
+    @abstractmethod
+    def remove_weather_station(self, user):
+        pass
+
+    @abstractmethod
+    def notify(self):
+        pass
+
+
+class WeatherStation(Subject):
     # document: https://pyowm.readthedocs.io/en/latest/pyowm.weatherapi25.html
     def __init__(self, owm_api_key=None):
         self._owm_api_key = owm_api_key
         self._owm = None
+        self.user_models = {}
+
+    def register_weather_station(self, user, model):
+        self.user_models.setdefault(user, model)
+
+    def remove_weather_station(self, user):
+        if self.user_models.get(user):
+            del self.user_models[user]
+        else:
+            logger.warning('remove_weather_station fail with del unexist user: {}'.format(user))
+
+    def notify(self):
+        for model in self.user_models.values():
+            model.notify(self.get_data_by_coord(
+                lon=model.user_data.get('longitude', 0), lat=model.user_data.get('latitude', 0)))
 
     @property
     def owm(self):
@@ -32,47 +59,3 @@ class WeatherStation():
         observation = self.owm.weather_at_coords(lat=lat, lon=lon)
         weather = observation.get_weather()
         return json.loads(weather.to_JSON())
-
-
-def trans_temp_kelvin_to_Celsius(temp):
-    return int(temp - 273.15)
-
-
-def enrich_general_model(weather_data):
-    return {
-        'temperature': trans_temp_kelvin_to_Celsius(
-            weather_data.get('temperature', {}).get('temp')),
-        'humidity': weather_data.get('humidity'),
-        'rain': weather_data.get('rain') if weather_data.get('rain') else 0
-    }
-
-
-def send_message(token, msg):
-    headers = {'Authorization': 'Bearer ' + token}
-    payload = {'message': str(msg)}
-    response = requests.post(LINE_URL, headers=headers, params=payload)
-    return response.status_code
-
-
-def send_general_model_message(token, msg):
-    logger.info('GeneralModel notify message: {}'.format(msg))
-    send_message(token, msg)
-
-
-def main():
-    config = get_json_content(path.join(CURRENT_PATH, 'config.json'))
-    for user, user_info in config['users'].items():
-        weather_station = WeatherStation(owm_api_key=config.get('owm_api_key'))
-        weather_data = weather_station.get_data_by_coord(
-            lon=user_info.get('longitude', 0), lat=user_info.get('latitude', 0))
-        logger.info('weather station get user {} lon: {}, lat:{} data:{}'.format(
-            user, user_info.get('longitude', 0), user_info.get('latitude', 0), weather_data))
-        general_model_msg = enrich_general_model(weather_data)
-        send_general_model_message(user_info.get('line_token'), general_model_msg)
-
-
-if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s %(levelname)s %(message)s')
-    main()
