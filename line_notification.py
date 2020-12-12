4719a4bff909fb5e1d7c448a413e5119244b77cf
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2020-12-12 08:06:52 +0800

    set base notifiaction and make LineNotifiacation inherit it

--- line_notification.py
+++ line_notification.py
@@ -1,30 +1,27 @@
 import requests
 import logging
-from abc import ABC, abstractmethod
+from base_notification import BaseNotificationInterface
 
 from utils import trans_temp_kelvin_to_Celsius
+
 
 logger = logging.getLogger(__name__)
 
 
-class LineNotificationInterface(ABC):
+class LineNotification(BaseNotificationInterface):
     LINE_URL = 'https://notify-api.line.me/api/notify'
 
     def __init__(self, line_token):
         self._line_token = line_token
 
-    @abstractmethod
     def notify(self, msg):
         headers = {'Authorization': 'Bearer ' + self._line_token}
         payload = {'message': str(msg)}
         response = requests.post(self.LINE_URL, headers=headers, params=payload)
         return response.status_code
 
-    def enrich_message(self, msg):
-        pass
 
-
-class GeneralModelNotification(LineNotificationInterface):
+class GeneralModelNotification(LineNotification):
     def __init__(self, weather_station, user_data):
         self.user_data = user_data
         weather_station.register_weather_station(user_data.get('user'), self)
@@ -44,7 +41,7 @@ class GeneralModelNotification(LineNotificationInterface):
         })
 
 
-class PremiumModelNotification(LineNotificationInterface):
+class PremiumModelNotification(LineNotification):
     def __init__(self, weather_station, user_data):
         self.user_data = user_data
         weather_station.register_weather_station(user_data.get('user'), self)
