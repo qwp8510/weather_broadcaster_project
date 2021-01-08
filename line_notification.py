b41040c20a1520b97f467c8a9a306a1aca425038
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-08 22:33:08 +0800

    add PrecipitationModelNotification model for Precipitation weather broadcaster

--- line_notification.py
+++ line_notification.py
@@ -30,14 +30,14 @@ class GeneralModelNotification(LineNotification):
     def notify(self, msg):
         logger.info('user: {} GeneralModel notify message: {}'.format(
             self.user_data.get('user', 'NAME_IS_NEEDED'), msg))
-        return super().notify(self.enrich_message(msg))
+        return super().notify(self._enrich_message(msg))
 
-    def enrich_message(self, msg):
+    def _enrich_message(self, msg):
         return str({
             'temperature': trans_temp_kelvin_to_Celsius(
                 msg.get('temperature', {}).get('temp', 'Null')),
             'humidity': msg.get('humidity', 'Null'),
-            'rain': msg.get('rain') if msg.get('rain') else 0
+            'status': msg.get('status', 'Null'),
         })
 
 
@@ -50,14 +50,42 @@ class PremiumModelNotification(LineNotification):
     def notify(self, msg):
         logger.info('user: {} PremiumModel notify message: {}'.format(
             self.user_data.get('user', 'NAME_IS_NEEDED'), msg))
-        return super().notify(self.enrich_message(msg))
+        return super().notify(self._enrich_message(msg))
 
-    def enrich_message(self, msg):
+    def _enrich_message(self, msg):
         return str({
             'temperature': trans_temp_kelvin_to_Celsius(
                 msg.get('temperature', {}).get('temp', 'Null')),
             'humidity': msg.get('humidity', 'Null'),
-            'rain': msg.get('rain') if msg.get('rain') else 0,
-            'status': msg.get('status', 'Null'),
+            'detailed_status': msg.get('detailed_status', 'Null'),
             'wind speed': msg.get('wind', {}).get('speed', 'Null')
         })
+
+
+class PrecipitationModelNotification(LineNotification):
+    precipitation_status = ['Drizzle', 'Rain', 'Thunderstorm']
+
+    def __init__(self, weather_station, user_data):
+        self.user_data = user_data
+        self.weather_station = weather_station
+        self.weather_station.register_weather_station(user_data.get('user'), self)
+        super().__init__(line_token=user_data.get('line_token'))
+
+    def notify(self, msg):
+        if self._valid_precipitation_probability(msg.get('status', 'Null')):
+            self.weather_station.remove_weather_station(self.user_data.get('user'))
+            logger.info('user: {} GeneralModel notify message: {}'.format(
+                self.user_data.get('user', 'NAME_IS_NEEDED'), self._enrich_message(msg)))
+            return super().notify(self._enrich_message(msg))
+
+    def _enrich_message(self, msg):
+        return str({
+            'temperature': trans_temp_kelvin_to_Celsius(
+                msg.get('temperature', {}).get('temp', 'Null')),
+            'humidity': msg.get('humidity', 'Null'),
+            'detailed_status': msg.get('detailed_status', 'Null'),
+            'rain': msg.get('rain') if msg.get('rain') else 'undescriptive message'
+        })
+
+    def _valid_precipitation_probability(self, status):
+        return status in self.precipitation_status
