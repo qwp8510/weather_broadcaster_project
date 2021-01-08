8bc98fd9c3a2c02a8eb2ce997f10e40592c629a4
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-08 22:39:25 +0800

    add precipitation_weather_broadcaster_broker.py

---
+++ precipitation_weather_broadcaster_broker.py
@@ -0,0 +1,42 @@
+from os import path
+import logging
+from time import sleep
+
+from utils import get_json_content
+from weather_broadcaster import WeatherBroadcaster
+from model import get_notification_model
+
+
+logger = logging.getLogger(__name__)
+CURRENT_PATH = path.dirname(path.abspath(__file__))
+time = 15 * 60
+
+
+def main():
+    user_data = get_json_content(path.join(CURRENT_PATH, 'user_data.json'))
+    weather_station = WeatherBroadcaster(owm_api_key=user_data.get('owm_api_key'))
+    for user, user_info in user_data['users'].items():
+        try:
+            if user_info.get('model_type') == 'PrecipitationModelNotification':
+                model = get_notification_model(user_info.get('model_type'))
+                model(weather_station, user_info)
+        except Exception as err:
+            logger.error('fail with model {} error: {}'.format(
+                user_info.get('model_type'), err))
+    while True:
+        try:
+            weather_station.notify()
+            sleep(time)
+        except KeyboardInterrupt:
+            logger.warning('keyboard interrupt\n')
+            break
+        except Exception as e:
+            logger.error('main exception: {}'.format(e))
+            break
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(message)s')
+    main()
